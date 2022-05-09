3b5a637fd71e6596638f28933ed300418473f7c4
Arkhipov <72arkhipov@mail.ru>
2022-05-09 20:24:34 +0500

    add Unique In Order.js

--- 6 kyu/Unique In Order.js
+++ 6 kyu/Unique In Order.js
@@ -6,7 +6,7 @@
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 var uniqueInOrder = function (iterable) {
-	let prev
+	let prev;
 	let arr = []
 	for (let key of iterable) {
 		if (prev !== key) {

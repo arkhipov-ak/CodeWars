6fca90ac8630ef64d7975f5e8248525d7c5726e8
Arkhipov <72arkhipov@mail.ru>
2022-05-10 19:50:53 +0500

    Name Shuffler

---
+++ 8 kyu/Name Shuffler.js
@@ -0,0 +1,6 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+function nameShuffler(str) {
+	return str.split(' ').reverse().join(' ')
+}

e2f57bc14af38227c487fc5763636a884949207d
Arkhipov <72arkhipov@mail.ru>
2022-05-15 17:55:58 +0500

    add Break camcelCase.js

---
+++ 6 kyu/Break camelCase.js
@@ -0,0 +1,14 @@
+// Завершите решение, чтобы функция разбивала верблюжий регистр, используя пробелы между словами.
+
+// Пример
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(string) {
+	string = string.split('').map(element => {
+		if(element = element.toUpperCase()) element = ' ' + element;
+		return element;
+	});
+	return element.join('');
+};

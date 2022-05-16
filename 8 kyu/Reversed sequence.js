f4720b50835884af32184e169d47f814fd7f1c78
Arkhipov <72arkhipov@mail.ru>
2022-05-16 20:09:43 +0500

    add Reversed sequence.js

---
+++ 8 kyu/Reversed sequence.js
@@ -0,0 +1,7 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+	return Array(n).fill(0).map((elem, index) => n - index);
+};

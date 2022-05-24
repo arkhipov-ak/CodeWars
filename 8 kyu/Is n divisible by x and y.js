38cda9fc2ae8d2036b31db7ca8e9292c6f321a3e
Arkhipov <72arkhipov@mail.ru>
2022-05-24 18:41:30 +0500

    add new Solution - Is in divisible by x and y.js

--- 8 kyu/Is n divisible by x and y.js
+++ 8 kyu/Is n divisible by x and y.js
@@ -6,6 +6,10 @@
 // 3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
 // 4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
 
+// function isDivisible(n, x, y) {
+// 	return (n % y) + (n % x) == 0 ? true : false
+// }
+
 function isDivisible(n, x, y) {
-	return (n % y) + (n % x) == 0 ? true : false
+	return (n % y) + (n % x) == 0
 }

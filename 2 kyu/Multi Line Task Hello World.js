64174ca3b5ad6729e94f578877ea9b34913acb7b
Arkhipov <72arkhipov@mail.ru>
2022-06-13 22:28:54 +0500

    add Multi Line Task Hello World.js

---
+++ 2 kyu/Multi Line Task Hello World.js
@@ -0,0 +1,79 @@
+// You need to write a function f that returns the string Hello, world!.
+
+// Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+// Hint: It's possible to complete this in 28 lines only.
+
+f=
+''
+[
+'\
+t\
+r\
+i\
+m'
+][
+'\
+b\
+i\
+n\
+d'
+]`
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!`
+
+// INLINE:
+
+// f = ''['trim']['bind']`Hello, world!`
+
+// -----------------------------------------------------------------------
+
+// Second Solution -- 36 lines
+
+// 0[
+// c=
+// '\
+// c\
+// o\
+// n\
+// s\
+// t\
+// r\
+// u\
+// c\
+// t\
+// o\
+// r'
+// ][
+// c]
+// `
+// f=
+// _\
+// =>
+// '\
+// H\
+// e\
+// l\
+// l\
+// o\
+// ,\
+//  \
+// w\
+// o\
+// r\
+// l\
+// d\
+// !'
+// `(
+// )

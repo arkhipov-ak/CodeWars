23d68f50b6ed6636a477c6c67151066bb787f0aa
Arkhipov <72arkhipov@mail.ru>
2022-06-14 11:54:54 +0500

    add Kata Example Twist.js

---
+++ 8 kyu/Kata Example Twist.js
@@ -0,0 +1,12 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// First Solution
+
+let websites = new Array(1000).fill('codewars');
+
+// Second Solution
+
+// let websites = [];
+// while (websites.length < 1000) websites.push('codewars');

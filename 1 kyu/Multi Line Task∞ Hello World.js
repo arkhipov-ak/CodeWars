95c1be79d08d854dc3cd62943bab55697c7fd855
Arkhipov <72arkhipov@mail.ru>
2022-06-14 11:23:38 +0500

    add Multi Line Task\342\210\236 Hello World.js

---
+++ 1 kyu/Multi Line Task∞ Hello World.js
@@ -0,0 +1,256 @@
+// You need to write a function f that returns the string Hello, world!.
+
+// Requirement: Every line must have at most 1 character, and total number of lines must be less than 145.
+
+// Hint: It's possible to complete this in less than 99 (tsukumo) lines.
+
+// Trivia: While ∞ can be pronounced as infinity, it's preferred to be pronounced as mugen here. It's the same, but way cooler.
+
+[
+,
+j
+,
+,
+o
+,
+,
+i
+,
+,
+n
+,
+,
+b
+,
+,
+d
+,
+,
+s
+,
+,
+p
+,
+,
+l
+,
+,
+t
+]
+=
+`
+j
+o
+i
+n
+b
+d
+s
+p
+l
+t
+`
+f
+=
+[
+]
+[
+j
++
+o
++
+i
++
+n
+]
+[
+b
++
+i
++
+n
++
+d
+]
+(
+`
+H
+e
+l
+l
+o
+,
+ 
+w
+o
+r
+l
+d
+!
+`
+[
+s
++
+p
++
+l
++
+i
++
+t
+]
+`
+`
+,
+[
+]
++
+[
+]
+)
+
+
+// SECOND SOLUTION - 143 LINES
+
+// [
+// ,
+// t
+// ,
+// ,
+// r
+// ,
+// ,
+// i
+// ,
+// ,
+// m
+// ,
+// ,
+// b
+// ,
+// ,
+// i
+// ,
+// ,
+// n
+// ,
+// ,
+// d
+// ,
+// ,
+// H
+// ,
+// ,
+// e
+// ,
+// ,
+// l
+// ,
+// ,
+// l
+// ,
+// ,
+// o
+// ,
+// ,
+// c
+// ,
+// ,
+// s
+// ,
+// ,
+// w
+// ,
+// ,
+// o
+// ,
+// ,
+// r
+// ,
+// ,
+// l
+// ,
+// ,
+// d
+// ,
+// ,
+// x
+// ]
+// =
+// `
+// t
+// r
+// i
+// m
+// b
+// i
+// n
+// d
+// H
+// e
+// l
+// l
+// o
+// ,
+ 
+// w
+// o
+// r
+// l
+// d
+// !
+// `
+// f
+// =
+// (
+// [
+// ]
+// +
+// [
+// ]
+// )
+// [
+// t
+// +
+// r
+// +
+// i
+// +
+// m
+// ]
+// [
+// b
+// +
+// i
+// +
+// n
+// +
+// d
+// ]
+// (
+// H
+// +
+// e
+// +
+// l
+// +
+// l
+// +
+// o
+// +
+// c
+// +
+// s
+// +
+// w
+// +
+// o
+// +
+// r
+// +
+// l
+// +
+// d
+// +
+// x
+// )

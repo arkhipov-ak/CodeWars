d390dfc9f0cde96188d083a684436f1b568ca82f
Arkhipov <72arkhipov@mail.ru>
2022-06-27 18:00:11 +0500

    add Failed Filter - Bug Fixing #3.js

---
+++ 8 kyu/Failed Filter - Bug Fixing #3.js
@@ -0,0 +1,6 @@
+// Failed Filter - Bug Fixing #3
+// Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the FilterNumber function to remove all the numbers from the string
+
+var FilterNumbers = function(str) {
+  return str.split('').filter(c => isNaN(+c)).join('');
+}

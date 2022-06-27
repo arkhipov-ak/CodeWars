8d37b4af47497dacede6e41752a46371e9174c4d
Arkhipov <72arkhipov@mail.ru>
2022-06-27 18:12:11 +0500

    add Class conundrum - Bug Fixing #7.js

---
+++ 8 kyu/Class conundrum - Bug Fixing #7.js
@@ -0,0 +1,25 @@
+// Class conundrum - Bug Fixing #7
+// Oh no! Timmy's List Class has broken! Can you help timmy and fix his class? Timmy has a List class he has created, this is used for type strict arrays (which timmy calls Lists).
+
+// When timmy calls the Count property of the list it still remains at 0 when adding items.
+
+// Also it fails when timmy trys to chain the adds e.g.
+
+// myList.add(0).add(1)
+
+class List {
+	constructor(type) {
+		this.type = type
+		this.items = []
+		this.count = 0
+	}
+
+	add(item) {
+		if (typeof item != this.type)
+			return `This item is not of type: ${this.type}`
+
+		this.items.push(item)
+		this.count++
+		return this
+	}
+}

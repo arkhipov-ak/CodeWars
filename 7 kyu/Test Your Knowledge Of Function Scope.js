d95f3f0d4dc5114fb4baaecdae0649dc8e6b1a52
Arkhipov <72arkhipov@mail.ru>
2022-06-30 10:36:54 +0500

    add Test Your Knowledge Of Function Scope.js

---
+++ 7 kyu/Test Your Knowledge Of Function Scope.js
@@ -0,0 +1,14 @@
+// Write a function that adds from two invocations.
+
+// All inputs will be integers.
+
+// add(3)(4)  // 7
+// add(12)(20) // 32
+
+const add = a => b => a + b;
+
+// function add(x) {
+// 	return function (y) {
+// 		return x + y
+// 	}
+// }

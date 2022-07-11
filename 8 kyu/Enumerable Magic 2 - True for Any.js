0030477b21b0879e0f01846380b4a4aaa5832a18
Arkhipov <72arkhipov@mail.ru>
2022-07-11 21:13:34 +0500

    add Enumerable Magic 2 - True for Any.js

---
+++ 8 kyu/Enumerable Magic 2 - True for Any.js
@@ -0,0 +1,5 @@
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+// Ruby: If you get stuck, you can read up here:
+// http://www.rubycuts.com/enum-any
+
+const any = (arr, fun) => arr.some(fun)

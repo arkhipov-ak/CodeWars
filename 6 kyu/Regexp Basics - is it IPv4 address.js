02284338323cdcbf51c9cc376b84f525872b0759
Arkhipov <72arkhipov@mail.ru>
2022-07-12 10:48:30 +0500

    add Regexp Basics - is it IPv4 address.js

--- 6 kyu/Regexp Basics - is it IPv4 address.js
+++ 6 kyu/Regexp Basics - is it IPv4 address.js
@@ -1,7 +1,8 @@
+
 // Implement String#ipv4_address?, which should return true if given object is an IPv4 address - four numbers (0-255) separated by dots.
 
 // It should only accept addresses in canonical representation, so no leading 0s, spaces etc.
 
-String.prototype.ipv4Address=function(){
-  return /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/.test(this)
+String.prototype.ipv4Address = function () {
+	return /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$/.test(this)
 }

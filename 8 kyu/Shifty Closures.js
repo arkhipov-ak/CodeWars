8c88f858c910d9bdca0c797689f5270ab6088f03
Arkhipov <72arkhipov@mail.ru>
2022-07-30 10:52:07 +0500

    add Shifty Closures.js

---
+++ 8 kyu/Shifty Closures.js
@@ -0,0 +1,6 @@
+// Functional closures can get overly attached. Set them straight!
+
+// Why doesn't greet_abe() actually greet Abe?
+
+const greet_abe = () => 'Hello, Abe!'
+const greet_ben = () => 'Hello, Ben!'

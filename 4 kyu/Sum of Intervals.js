262595e12bb6d481629d657a730d110c6997c837
Alexandr <72arkhipov@mail.ru>
2022-09-03 19:47:06 +0500

    add Sum of Intervals.js

---
+++ 4 kyu/Sum of Intervals.js
@@ -0,0 +1,53 @@
+// Write a function called sumIntervals/sum_intervals() that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+// Intervals
+// Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+// Overlapping Intervals
+// List containing overlapping intervals:
+
+// [
+//    [1,4],
+//    [7, 10],
+//    [3, 5]
+// ]
+// The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+// Examples:
+// sumIntervals( [
+//    [1,2],
+//    [6, 10],
+//    [11, 15]
+// ] ) => 9
+
+// sumIntervals( [
+//    [1,4],
+//    [7, 10],
+//    [3, 5]
+// ] ) => 7
+
+// sumIntervals( [
+//    [1,5],
+//    [10, 20],
+//    [1, 6],
+//    [16, 19],
+//    [5, 11]
+// ] ) => 19
+
+// sumIntervals( [
+//    [0, 20],
+//    [-100000000, 10],
+//    [30, 40]
+// ] ) => 100000030
+// Tests with large intervals
+// Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
+
+const sumIntervals = intervals =>
+	intervals
+		.concat()
+		.sort((a, b) => a[0] - b[0])
+		.reduce(
+			(a, c) =>
+				c[1] <= a[1] ? a : [a[0] + Math.abs(c[1] - Math.max(a[1], c[0])), c[1]],
+			[0, -Infinity]
+		)[0]

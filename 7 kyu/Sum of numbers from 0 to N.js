ee5c591871db6af487dbf77ba96bbed903853913
HODOR72 <72arkhipov@mail.ru>
2022-10-12 14:17:37 +0300

    add Sum of numbers from 0 to N.js

---
+++ 7 kyu/Sum of numbers from 0 to N.js
@@ -0,0 +1,45 @@
+// Description:
+
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+// Example:
+// Input:
+
+// > 6
+// Output:
+
+// 0+1+2+3+4+5+6 = 21
+
+// Input:
+
+// > -15
+// Output:
+
+// -15<0
+
+// Input:
+
+// > 0
+// Output:
+
+// 0=0
+
+var SequenceSum = (function () {
+	function SequenceSum() {}
+
+	SequenceSum.showSequence = function (count) {
+		var m = [],
+			s = 0
+		if (count === 0) return count + '=' + 0
+		if (count < 0) return count + '<' + 0
+
+		for (var i = 0; i <= count; i++) {
+			s += i
+			m.push(i)
+		}
+
+		return m.join('+') + ' = ' + s
+	}
+
+	return SequenceSum
+})()
